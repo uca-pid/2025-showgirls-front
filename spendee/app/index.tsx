ee02f8d6ec30f42e1798323a5245354c19bee00e
Jorge_N <valentinnave@gmail.com>
2025-09-11 12:00:56 -0300

    add sign up form component and sign up page

--- spendee/app/index.tsx
+++ spendee/app/index.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native"
+import { router } from "expo-router"
+import { Pressable, Text, View } from "react-native"
 
 export default function HomeScreen() {
   return (
-    <View className='flex-1 items-center justify-center bg-red-300'>
-      <Text>Welcome!</Text>
+    <View className='flex-1 items-center justify-center bg-background'>
+      <Text className='color-white'>Welcome!</Text>
+      <Pressable
+        onPress={() => {
+          router.push("/sign-in/SignInPage")
+        }}
+      >
+        <Text className='color-white'>Go to login</Text>
+      </Pressable>
     </View>
   )
 }
---
+++ spendee/components/sign-in-form.tsx
@@ -0,0 +1,107 @@
+import { SocialConnections } from "@/components/social-connections"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Separator } from "@/components/ui/separator"
+import { Text } from "@/components/ui/text"
+import * as React from "react"
+import { Pressable, type TextInput, View } from "react-native"
+
+export function SignInForm() {
+  const passwordInputRef = React.useRef<TextInput>(null)
+
+  function onEmailSubmitEditing() {
+    passwordInputRef.current?.focus()
+  }
+
+  function onSubmit() {
+    // TODO: Submit form and navigate to protected screen if successful
+  }
+
+  return (
+    <View className='gap-6'>
+      <Card className='border-border/0 sm:border-border shadow-none sm:shadow-sm sm:shadow-black/5'>
+        <CardHeader>
+          <CardTitle
+            className='text-center text-2xl sm:text-left'
+            testID='loginTitle'
+          >
+            Iniciar sesión
+          </CardTitle>
+          <CardDescription className='text-center sm:text-left'>
+            ¡Bienvenido! Iniciá sesión para continuar
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='gap-6'>
+          <View className='gap-6'>
+            <View className='gap-1.5'>
+              <Label htmlFor='email'>Usuario</Label>
+              <Input
+                id='email'
+                placeholder='m@example.com'
+                keyboardType='email-address'
+                autoComplete='email'
+                autoCapitalize='none'
+                onSubmitEditing={onEmailSubmitEditing}
+                returnKeyType='next'
+                submitBehavior='submit'
+              />
+            </View>
+            <View className='gap-1.5'>
+              <View className='flex-row items-center'>
+                <Label htmlFor='password'>Contraseña</Label>
+                <Button
+                  variant='link'
+                  size='sm'
+                  className='web:h-fit ml-auto h-4 px-1 py-0 sm:h-4'
+                  onPress={() => {
+                    // TODO: Navigate to forgot password screen
+                  }}
+                >
+                  <Text className='font-normal leading-4'>
+                    Olvidé mi contraseña
+                  </Text>
+                </Button>
+              </View>
+              <Input
+                ref={passwordInputRef}
+                id='password'
+                secureTextEntry
+                returnKeyType='send'
+                onSubmitEditing={onSubmit}
+              />
+            </View>
+            <Button className='w-full' onPress={onSubmit}>
+              <Text>Continuar</Text>
+            </Button>
+            <View className='flex-row items-center'>
+              <Text className='text-sm'>¿No tenés cuenta? </Text>
+              <Pressable
+                onPress={() => {
+                  // TODO: Navigate to sign up screen
+                }}
+              >
+                <Text className='text-sm underline underline-offset-4'>
+                  Registrate
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+          <View className='flex-row items-center'>
+            <Separator className='flex-1' />
+            <Text className='text-muted-foreground px-4 text-sm'>or</Text>
+            <Separator className='flex-1' />
+          </View>
+          <SocialConnections />
+        </CardContent>
+      </Card>
+    </View>
+  )
+}
