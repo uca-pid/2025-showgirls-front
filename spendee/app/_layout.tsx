3804273add65f3442b391dbbf1a3b487da9a8bea
Jorge_N <valentinnave@gmail.com>
2025-09-10 23:50:11 -0300

    setup react native reusables

--- spendee/app/_layout.tsx
+++ spendee/app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from "expo-router"
 import "../global.css"
+import { PortalHost } from "@rn-primitives/portal"
+import React from "react"
 
 export default function RootLayout() {
-  return <Stack />
+  return (
+    <>
+      <Stack />
+      <PortalHost />
+    </>
+  )
 }
