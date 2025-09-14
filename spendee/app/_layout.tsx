7abc813f398c617c60696fec953282d672429699
Jorge_N <valentinnave@gmail.com>
2025-09-14 10:53:26 -0300

    fix: change toast theme

--- spendee/app/_layout.tsx
+++ spendee/app/_layout.tsx
@@ -13,37 +13,44 @@ import {
   ThemeProvider,
 } from "@react-navigation/native"
 import { useColorScheme } from "react-native"
-import { Info } from "lucide-react-native"
-
-const toastConfig = {
-  danger: (props: BaseToastProps) => (
-    <BaseToast
-      {...props}
-      renderLeadingIcon={() => <Info />}
-      style={{
-        marginTop: 30,
-        borderLeftColor: "rgba(194, 3, 3, 1)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-      contentContainerStyle={{}}
-      text1Style={{
-        fontSize: 17,
-        fontWeight: "300",
-        color: "rgba(194, 3, 3, 1)",
-      }}
-      text2Style={{
-        fontSize: 15,
-        fontWeight: "400",
-        color: "white",
-      }}
-      text1NumberOfLines={2}
-    />
-  ),
-}
+import { Info, OctagonAlert } from "lucide-react-native"
 
 export default function RootLayout() {
+  const toastConfig = {
+    danger: (props: BaseToastProps) => (
+      <BaseToast
+        {...props}
+        renderLeadingIcon={() => (
+          <OctagonAlert color={"rgba(194, 3, 3, 1)"} size={20} />
+        )}
+        style={{
+          marginTop: 30,
+          borderLeftColor: "rgba(194, 3, 3, 1)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: theme === DarkTheme ? "black" : "white",
+          paddingLeft: 20,
+        }}
+        contentContainerStyle={{
+          display: "flex",
+          alignItems: "center",
+          alignSelf: "center",
+        }}
+        text1Style={{
+          fontSize: 17,
+          fontWeight: "300",
+          color: "rgba(194, 3, 3, 1)",
+        }}
+        text2Style={{
+          fontSize: 15,
+          fontWeight: "400",
+          color: "white",
+        }}
+        text1NumberOfLines={2}
+      />
+    ),
+  }
   const colorScheme = useColorScheme()
   const theme = colorScheme === "dark" ? DarkTheme : DefaultTheme
   return (
