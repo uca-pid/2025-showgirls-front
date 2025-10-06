88e2a8848f398a6ca837aacd23e3325ad30b9f72
Jorge_N <valentinnave@gmail.com>
2025-10-06 19:24:04 -0300

    add query client provider

--- spendee/app/_layout.tsx
+++ spendee/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { router, Stack } from 'expo-router'
 import '@/global.css'
 import { PortalHost } from '@rn-primitives/portal'
-import React, { useEffect } from 'react'
+import React, { use, useEffect } from 'react'
 import {
   DarkTheme,
   DefaultTheme,
@@ -12,10 +12,12 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../firebase.config'
 import { AuthProvider } from '@/context/AuthContext'
 import { ToastProvider } from '@/context/ToastContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
   const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme
+  const queryClient = new QueryClient()
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -30,22 +32,48 @@ export default function RootLayout() {
   }, [])
 
   return (
-    <ToastProvider>
+    <ThemeProvider value={theme}>
       <AuthProvider>
-        <ThemeProvider value={theme}>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              animation: 'fade',
-              animationDuration: 200,
-            }}
-          >
-            <Stack.Screen name="sign-in/index" />
-            <Stack.Screen name="(tabs)" />
-          </Stack>
-          <PortalHost />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ToastProvider>
+            <Stack>
+              <Stack.Screen
+                name="sign-in/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="sign-up/index"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="edit-profile/index"
+                options={{ title: 'Editar Perfil', headerBackTitle: 'Perfil' }}
+              />
+              <Stack.Screen
+                name="edit-profile/change-password"
+                options={{
+                  title: 'Cambiar Contraseña',
+                  headerBackTitle: 'Volver',
+                }}
+              />
+              <Stack.Screen
+                name="expense/index"
+                options={{ title: 'Mis Gastos' }}
+              />
+              <Stack.Screen
+                name="expense/modal"
+                options={{ headerShown: false, presentation: 'modal' }}
+              />
+              <Stack.Screen
+                name="expense/[id]"
+                options={{ title: 'Detalle del Gasto' }}
+              />
+            </Stack>
+          </ToastProvider>
+        </QueryClientProvider>
       </AuthProvider>
-    </ToastProvider>
+      <PortalHost />
+    </ThemeProvider>
   )
 }
