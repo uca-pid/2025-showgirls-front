7bb39f5388e5205b0fc327b8365a08f95fb40866
Jorge_N <valentinnave@gmail.com>
2025-10-07 16:27:59 -0300

    add chartData hook

---
+++ Spendee/hooks/useChartData.ts
@@ -0,0 +1,79 @@
+// hooks/useChartData.ts
+import categoryService from '@/services/category.service'
+import { useQuery } from '@tanstack/react-query'
+import {
+  BookOpen,
+  Bus,
+  Ellipsis,
+  Gamepad2,
+  Heart,
+  House,
+  LucideIcon,
+  Paperclip,
+  Popcorn,
+  Shield,
+  Shuffle,
+  Sigma,
+  Sprout,
+  Sun,
+  TestTube,
+  TreePalm,
+  Users,
+  Utensils,
+  Wine,
+  Wrench,
+} from 'lucide-react-native'
+
+const iconNameToEmoji: Record<string, LucideIcon> = {
+  bus: Bus,
+  utensils: Utensils,
+  home: House,
+  heart: Heart,
+  gamepad: Gamepad2,
+  book: BookOpen,
+  Wrench: Wrench,
+  Wine: Wine,
+  Sprout: Sprout,
+  Users: Users,
+  TreePalm: TreePalm,
+  TestTube: TestTube,
+  Sun: Sun,
+  Sigma: Sigma,
+  Popcorn: Popcorn,
+  Shuffle: Shuffle,
+  Shield: Shield,
+  Paperclip: Paperclip,
+  '': Ellipsis,
+  'ellipsis-h': Ellipsis,
+}
+
+export interface ChartData {
+  categoryIcon: LucideIcon
+  value: number
+  categoryColor: string
+  categoryName: string
+  categoryId: number
+}
+
+export default function useChartData() {
+  const {
+    data: chartData = [],
+    isLoading,
+    refetch,
+    ...rest
+  } = useQuery<ChartData[]>({
+    queryKey: ['categoriesChart'],
+    queryFn: async () => {
+      const res = await categoryService.findMany()
+      return res.data.map((item: any) => ({
+        categoryIcon: iconNameToEmoji[item.icono] || Ellipsis,
+        value: Number(item.totalGastos ?? 0),
+        categoryColor: item.color ?? '#000000',
+        categoryName: item.nombre ?? 'Sin nombre',
+        categoryId: item.id,
+      }))
+    },
+  })
+
+  return { chartData, isLoading, refetch, ...rest }
+}
