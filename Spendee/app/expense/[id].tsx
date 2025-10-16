6b8bac08b8bbde7eb46b44faaf7feb98ebc63dcb
Jorge_N <valentinnave@gmail.com>
2025-10-16 01:27:17 -0300

    expense detail page ui

--- Spendee/app/expense/[id].tsx
+++ Spendee/app/expense/[id].tsx
@@ -1,25 +1,29 @@
-import { Button, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { router, useGlobalSearchParams } from 'expo-router'
-import expenseService, { ExpenseResponse } from '../../services/expense.service'
-import { Card, CardContent, CardTitle } from '@/components/ui/card'
+import Container from '@/components/Container'
+import Section from '@/components/Section'
+import SectionCard from '@/components/SectionCard'
+import { Icon } from '@/components/ui/icon'
+import { Text } from '@/components/ui/text'
+import useCategories from '@/hooks/useCategories'
+import useExpenseDetail from '@/hooks/useExpenseDetail'
+import { getIcon } from '@/lib/getIcon'
+import { Link, router, useGlobalSearchParams } from 'expo-router'
+import { ChevronRight } from 'lucide-react-native'
+import React from 'react'
+import { View } from 'react-native'
+import expenseService from '../../services/expense.service'
 
 const ExpenseDetailPage = () => {
-  const [expense, setExpense] = useState<ExpenseResponse>()
   const { id } = useGlobalSearchParams()
+  const { expenseDetailData } = useExpenseDetail(Number(id))
+  const { categoriesData } = useCategories()
 
-  useEffect(() => {
-    const getExpense = async () => {
-      const res = await expenseService.findByExpenseId(Number(id))
-      setExpense(res.data)
-    }
-    getExpense()
-  }, [])
+  const category = categoriesData.find(
+    (cat) => cat.id === expenseDetailData.categoriaId,
+  )
 
   async function handleDeleteExpense() {
     if (!id) return
 
-    // Try to confirm in environments that support a confirmation dialog (web).
     const confirmed =
       typeof (global as any).confirm === 'function'
         ? (global as any).confirm('¿Desea eliminar este gasto?')
@@ -41,24 +45,79 @@ const ExpenseDetailPage = () => {
         throw new Error('Delete method not available on expenseService')
       }
 
-      // Navigate back after successful deletion
       router.back()
     } catch (error) {
       console.error('Failed to delete expense:', error)
-      // You might want to show a user-facing error UI here
     }
   }
-  
-  return (
-    <View className="bg-background w-full h-full items-center p-4">
-      <Card className="w-full">
-        <CardContent className="flex-row justify-between items-center">
-          <CardTitle>{expense?.fecha ? new Date(expense.fecha).toLocaleDateString() : 'Sin Fecha'} ${expense?.gasto}</CardTitle>
-          <Button title="Eliminar" onPress={() => {handleDeleteExpense()}} />
-        </CardContent>
 
-      </Card>
-    </View>
+  return (
+    <Container>
+      <Section>
+        <SectionCard>
+          <Text className="text-muted-foreground">
+            {new Date(expenseDetailData.fecha).toLocaleDateString('es-ES', {
+              day: '2-digit',
+              month: 'long',
+              year: 'numeric',
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+          </Text>
+          <Text className="text-2xl font-semibold">
+            {'-$ ' + expenseDetailData.gasto}
+          </Text>
+        </SectionCard>
+      </Section>
+
+      <Section>
+        <SectionCard>
+          <View className="items-center flex-row">
+            <Text className="text-muted-foreground">Gasto en </Text>
+            <Text>{category?.nombre}</Text>
+          </View>
+          <Icon
+            as={getIcon(category ? category?.icono : 'ellipsis')}
+            size={24}
+            color={category?.color}
+          />
+          <View className="items-center flex-row gap-1">
+            <Link href="/category" asChild>
+              <Text className="text-muted-foreground">
+                Ver todas las categorías
+              </Text>
+            </Link>
+            <ChevronRight color="gray" size={20} />
+          </View>
+        </SectionCard>
+      </Section>
+
+      <Section>
+        <SectionCard>
+          <Text className="text-muted-foreground">Resultado del balance</Text>
+          <View className="flex-row w-full items-center justify-evenly">
+            <View className="items-center">
+              <Text className="text-muted-foreground">Antes</Text>
+              <Text adjustsFontSizeToFit>
+                {'$' + expenseDetailData.montoAnterior}
+              </Text>
+            </View>
+            <View className="flex-row">
+              <ChevronRight size={24} color={'gray'} />
+              <ChevronRight size={24} color={'gray'} />
+              <ChevronRight size={24} color={'gray'} />
+            </View>
+            <View className="items-center">
+              <Text className="text-muted-foreground">Después</Text>
+              <Text adjustsFontSizeToFit>
+                {'$' +
+                  (expenseDetailData.montoAnterior - expenseDetailData.gasto)}
+              </Text>
+            </View>
+          </View>
+        </SectionCard>
+      </Section>
+    </Container>
   )
 }
 
