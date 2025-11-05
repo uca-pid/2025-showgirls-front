f8f30f4300d32e274704ba498180d6a35adccabd
Jorge_N <valentinnave@gmail.com>
2025-11-05 12:11:31 -0300

    iconButton minor ui changes

--- Spendee/components/IconButton.tsx
+++ Spendee/components/IconButton.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 import { Pressable, Text, View } from 'react-native'
 
 export interface IconButtonProps {
-  text: string
+  text?: string
   textColor?: string
   icon: LucideIcon
+  iconColor?: string
   onPress?: () => void
   className?: string
 }
@@ -15,18 +16,24 @@ const IconButton = ({
   text,
   textColor = 'black',
   icon: Icon,
+  iconColor = '#F9A8D4',
   onPress,
   className,
 }: IconButtonProps) => {
   return (
-    <View className={`flex-col items-center ${text ? 'w-[100px]' : ''}`}>
+    <View className={`border flex-col items-center ${text ? 'w-[100px]' : ''}`}>
       <Pressable onPress={onPress} className="items-center">
-        <View className={cn('p-4 rounded-full bg-pink-300/30 mb-2', className)}>
-          <Icon color={'#F9A8D4'} />
+        <View
+          className={cn(`p-4 rounded-full ${text && 'mb-2'}`, className)}
+          style={{ backgroundColor: `${iconColor}30` }}
+        >
+          <Icon color={iconColor} />
         </View>
-        <Text className={`text-${textColor}`} numberOfLines={1}>
-          {text}
-        </Text>
+        {text && (
+          <Text className={`text-${textColor}`} numberOfLines={1}>
+            {text}
+          </Text>
+        )}
       </Pressable>
     </View>
   )
