d212e4ded8c000c0d68103cf29d87531b6609444
TriniSuarezC <trinisuarezcarreras@gmail.com>
2025-11-21 17:59:17 -0300

    levels page added

--- Spendee/app/level/index.tsx
+++ Spendee/app/level/index.tsx
@@ -1,9 +1,14 @@
-import { View } from 'react-native'
+import { FlatList, View } from 'react-native'
 import React from 'react'
 import LottieView from 'lottie-react-native'
 import { Text } from '@/components/ui/text'
+import useLevel from '@/hooks/useLevel'
+import { useAuth } from '@/context/AuthContext'
+import ItemCard from '@/components/ItemCard'
 
 const Level = () => {
+  const { user } = useAuth()
+  const { levelData, objectiveData } = useLevel(user?.uid ?? '')
   return (
     <View className="items-center">
       <LottieView
@@ -12,7 +17,17 @@ const Level = () => {
         loop={false}
         style={{ width: 250, height: 250 }}
       />
-      <Text className="text-lg font-semibold">Nivel: </Text>
+      <Text className="text-lg font-semibold">Nivel: {levelData?.level}</Text>
+      <FlatList
+        data={Array.isArray(objectiveData) ? (objectiveData as any[]) : []}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ItemCard
+            title={`Objetivo: ${item.id.toString()}`}
+            description={item.descripcion}
+          />
+        )}
+      />
     </View>
   )
 }
